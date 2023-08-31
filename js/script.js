f6394ef4557517dbae8b27d1c3b51bcd8351bbdd
moviefan322 <philman202@yahoo.com>
2023-08-31 09:12:19 -0400

    adds js to animate typing effect on header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 // Add custom JavaScript here
+function runTypingEffect() {
+  const text = "I am Philip Neumann.";
+  const typingElement = document.getElementById("typing-text");
+  const typingDelay = 100;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, typingDelay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, typingDelay * i);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", runTypingEffect);
